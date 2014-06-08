f11e3dbbdf6bf72297e378503e453ff74402c866
Marisa Yeung <marisayeung1@gmail.com>
2014-06-08 01:15:07 -0700

    Fixed plot3, added all plot code for plot4

--- plot3.R
+++ plot3.R
@@ -1,14 +1,14 @@
 with(energy, {
-  plot(energy$timestamp, energy$Sub_metering_1, 
+  plot(timestamp, energy$Sub_metering_1, 
        type="l", xlab = "", ylab = "Energy sub metering")
   
-  lines(energy$timestamp, energy$Sub_metering_2, 
+  lines(timestamp, energy$Sub_metering_2, 
        col = "red")
   
-  lines(energy$timestamp, energy$Sub_metering_3, 
+  lines(timestamp, energy$Sub_metering_3, 
        col = "blue")
   
   legend("topright", col = c("black", "red", "blue"), 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-       lty = c(1,1,1))
+       lty = c(1,1,1), bty="n")
 })
---
+++ plot4.R
@@ -0,0 +1,22 @@
+
+par(mfrow = c(2,2))
+
+with(energy, {plot(timestamp, Global_active_power, 
+                   type="l", xlab = "", ylab = "Global Active Power (kilowatts)")
+              
+              plot(timestamp, Voltage, type = "l", xlab = "datetime")
+              
+              {plot(timestamp, Sub_metering_1, 
+                     type="l", xlab = "", ylab = "Energy sub metering")
+               lines(timestamp, energy$Sub_metering_2, 
+                      col = "red")
+               lines(timestamp, energy$Sub_metering_3, 
+                      col = "blue")
+               legend("topright", col = c("black", "red", "blue"), 
+                       legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
+                       lty = c(1,1,1), bty="n")
+              }
+              
+              plot(timestamp, Global_reactive_power, 
+                   type="l", xlab = "datetime", ylab = "Global_reactive_power")
+              })
