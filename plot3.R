f1d287a8e08e97efc6f114eb655ff38a1ea40444
Marisa Yeung <marisayeung1@gmail.com>
2014-06-08 01:07:41 -0700

    Fixed plot 3 to use "with", started plot4

--- plot3.R
+++ plot3.R
@@ -1,12 +1,14 @@
-plot(energy$timestamp, energy$Sub_metering_1, 
-     type="l", xlab = "", ylab = "Energy sub metering")
-
-lines(energy$timestamp, energy$Sub_metering_2, 
+with(energy, {
+  plot(energy$timestamp, energy$Sub_metering_1, 
+       type="l", xlab = "", ylab = "Energy sub metering")
+  
+  lines(energy$timestamp, energy$Sub_metering_2, 
        col = "red")
-
-lines(energy$timestamp, energy$Sub_metering_3, 
+  
+  lines(energy$timestamp, energy$Sub_metering_3, 
        col = "blue")
-
-legend("topright", col = c("black", "red", "blue"), 
+  
+  legend("topright", col = c("black", "red", "blue"), 
        legend = c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
        lty = c(1,1,1))
+})
